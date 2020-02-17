9c86bb306dc71c7aea1d961b60f9d98bef7b7d41
Matthew Martin <zhintateo19924559@gmail.com>
2020-02-17 07:18:12 -0600

    completed firstNamesAllCaps

--- index.js
+++ index.js
@@ -210,8 +210,12 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const capFirst = [];
+  let capFirstName = runners.map(function(runners){
+    capFirst.push(`${runners.first_name.toUpperCase()}`);
+  });
+  return capFirst;
 }
 
 /**
